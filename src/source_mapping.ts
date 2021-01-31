fd2e9f0da2955509669f063cceddf6b73db7875d
yy0931 <54441600+yy0931@users.noreply.github.com>
2021-02-01 06:00:23 +0900

    SourceMappingの高速化
    1000行のコードに対して 361ms -> 50ms

--- src/source_mapping.ts
+++ src/source_mapping.ts
@@ -1,21 +1,33 @@
 import { Token, TokenWithSourceMap } from "./tokenize"
 
 class SourceMapping {
-    constructor(private readonly sourceCodeLength: number, private readonly preprocessed: { text: string; sourcePosition: number; }[]) {
+    private readonly preprocessed: { text: string; sourcePosition: number; preprocessedCodePosition: number }[]
+
+    constructor(private readonly sourceCodeLength: number, preprocessed: { text: string; sourcePosition: number; }[]) {
+        this.preprocessed = []
+        let i = 0
+        for (const el of preprocessed) {
+            this.preprocessed.push({ ...el, preprocessedCodePosition: i })
+            i += el.text.length
+        }
     }
 
     /** preprocess後の文字列上のoffsetからソースコード上のoffsetへ変換 */
     public map(offsetOnPreprocessedCode: number): number {
-        // TODO: 高速化
-        let j = offsetOnPreprocessedCode
         for (let i = 0; i < this.preprocessed.length - 1; i++) {
-            if (j < this.preprocessed[i].text.length) {
-                return Math.min(this.preprocessed[i].sourcePosition + j, this.preprocessed[i + 1].sourcePosition - 1)
+            if (offsetOnPreprocessedCode < this.preprocessed[i + 1].preprocessedCodePosition) {
+                return Math.min(
+                    this.preprocessed[i].sourcePosition + (offsetOnPreprocessedCode - this.preprocessed[i].preprocessedCodePosition),
+                    this.preprocessed[i + 1].sourcePosition - 1,
+                )
             }
-            j -= this.preprocessed[i].text.length
         }
+
         const last = this.preprocessed[this.preprocessed.length - 1]
-        return Math.min(last.sourcePosition + j, this.sourceCodeLength)
+        return Math.min(
+            last.sourcePosition + (offsetOnPreprocessedCode - last.preprocessedCodePosition),
+            this.sourceCodeLength,
+        )
     }
 }
 

3d9a18962a185c40889c40995134ba76f5dfaff6
yy0931 <54441600+yy0931@users.noreply.github.com>
2021-02-01 06:26:18 +0900

    SourceMappingを更に高速化
    1000行のコードに対して50ms -> 8ms

--- src/source_mapping.ts
+++ src/source_mapping.ts
@@ -2,6 +2,8 @@ import { Token, TokenWithSourceMap } from "./tokenize"
 
 class SourceMapping {
     private readonly preprocessed: { text: string; sourcePosition: number; preprocessedCodePosition: number }[]
+    private lastIndex = 0
+    private lastPreprocessedCodePosition = 0
 
     constructor(private readonly sourceCodeLength: number, preprocessed: { text: string; sourcePosition: number; }[]) {
         this.preprocessed = []
@@ -13,19 +15,27 @@ class SourceMapping {
     }
 
     /** preprocess後の文字列上のoffsetからソースコード上のoffsetへ変換 */
-    public map(offsetOnPreprocessedCode: number): number {
-        for (let i = 0; i < this.preprocessed.length - 1; i++) {
-            if (offsetOnPreprocessedCode < this.preprocessed[i + 1].preprocessedCodePosition) {
+    public map(preprocessedCodePosition: number): number {
+        // 連続アクセスに対する高速化
+        if (preprocessedCodePosition < this.lastPreprocessedCodePosition) {
+            this.lastIndex = 0
+        }
+        this.lastPreprocessedCodePosition = preprocessedCodePosition
+
+        for (let i = this.lastIndex; i < this.preprocessed.length - 1; i++) {
+            if (preprocessedCodePosition < this.preprocessed[i + 1].preprocessedCodePosition) {
+                this.lastIndex = i
                 return Math.min(
-                    this.preprocessed[i].sourcePosition + (offsetOnPreprocessedCode - this.preprocessed[i].preprocessedCodePosition),
+                    this.preprocessed[i].sourcePosition + (preprocessedCodePosition - this.preprocessed[i].preprocessedCodePosition),
                     this.preprocessed[i + 1].sourcePosition - 1,
                 )
             }
         }
 
+        this.lastIndex = this.preprocessed.length - 1
         const last = this.preprocessed[this.preprocessed.length - 1]
         return Math.min(
-            last.sourcePosition + (offsetOnPreprocessedCode - last.preprocessedCodePosition),
+            last.sourcePosition + (preprocessedCodePosition - last.preprocessedCodePosition),
             this.sourceCodeLength,
         )
     }

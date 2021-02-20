6a93cff2800e5b5b3e6ca16351298b4a38263ec8
yy0931 <54441600+yy0931@users.noreply.github.com>
2021-02-20 16:20:37 +0900

    なでしこ言語のバージョンを v3.1.18 に更新
    シンタックスハイライト以外の機能を一時的に削除

--- src/extension.ts
+++ src/extension.ts
@@ -1,373 +1,136 @@
 import * as vscode from 'vscode'
-import jsBeutify = require("js-beautify")
+import { LanguageFeatures, BackgroundTokenizer, AceDocument, TokenType } from 'nadesiko3/src/wnako3_editor'
+import * as NakoCompiler from 'nadesiko3/src/nako3'
 
-import * as fs from "fs"
-import * as path from "path"
-import * as util from 'util'
+class AceRange {
+	constructor(
+		public readonly startLine: number,
+		public readonly startColumn: number,
+		public readonly endLine: number,
+		public readonly endColumn: number,
+	) { }
+}
 
-import NakoCompiler = require('nadesiko3/src/nako3')
-import pluginNode = require("nadesiko3/src/plugin_node")
-import pluginCSV = require("nadesiko3/src/plugin_csv")
-import { lex, parse } from './nadesiko3/nako3'
-import { LexError } from './nadesiko3/nako_lexer'
-import { mockPlugins } from "./nako3_plugins"
+/**
+ * VSCodeのDocumentをAceのDocumentとして使う。
+ */
+class DocumentAdapter implements AceDocument {
+	constructor(private readonly editor: vscode.TextEditor) { }
+	getLine(row: number) { return this.editor.document.lineAt(row).text }
+	getAllLines() { return this.editor.document.getText().split('\n') }
+	getLength() { return this.editor.document.lineCount }
+	insertInLine(position: { row: number, column: number }, text: string) {
+		this.editor.edit((builder) => { builder.insert(new vscode.Position(position.row, position.column), text) })
+			.then(undefined, (err) => { console.error(err) })
+	}
+	removeInLine(row: number, columnStart: number, columnEnd: number) {
+		this.editor.edit((builder) => { builder.delete(new vscode.Range(row, columnStart, row, columnEnd)) })
+			.then(undefined, (err) => { console.error(err) })
+	}
+	replace(range: AceRange, text: string) {
+		this.editor.edit((builder) => { builder.replace(new vscode.Range(range.startLine, range.startColumn, range.endLine, range.endColumn), text) })
+			.then(undefined, (err) => { console.error(err) })
+	}
+}
 
-import WebNakoServer from './web_nako_server'
-import documentHighlightProvider from './language_features/document_highlight_provider'
-import codeLendsProvider from './language_features/code_lens'
-import DefinitionProvider from './language_features/definition_provider'
-import { createDeclarationFile } from './document'
-import * as abs from "./language_features/abstract_vscode"
-import updateDiagnostics from "./language_features/diagnostics"
-import LanguageFeatures from './language_features'
+type VSCodeTokenType = "namespace" | "class" | "enum" | "interface" | "struct" | "typeParameter" | "type" | "parameter" | "variable" | "property" | "enumMember" | "event" | "function" | "method" | "macro" | "label" | "comment" | "string" | "keyword" | "number" | "regexp" | "operator"
+
+const legend = new vscode.SemanticTokensLegend(
+	["namespace", "class", "enum", "interface", "struct", "typeParameter", "type", "parameter", "variable", "property", "enumMember", "event", "function", "method", "macro", "label", "comment", "string", "keyword", "number", "regexp", "operator"] as VSCodeTokenType[],
+	["declaration", "definition", "readonly", "static", "deprecated", "abstract", "async", "modification", "documentation", "defaultLibrary"])
+
+/**
+ * Aceのtoken typeをVSCodeのtoken typeにマップする。
+ */
+const mapTokenType = (type: TokenType): [VSCodeTokenType, string[]] | null => {
+	switch (type) {
+		case "comment.block": return ["comment", []]
+		case "comment.line": return ["comment", []]
+		case "constant.numeric": return ["number", []]
+		case "entity.name.function": return ["function", []]
+		case "keyword.control": return ["keyword", []]
+		case "keyword.operator": return ["operator", []]
+		case "markup.other": return null
+		case "string.other": return ["string", []]
+		case "support.constant": return ["variable", ["readonly"]]
+		case "variable.language": return ["macro", []]
+		case "variable.other": return ["variable", []]
+		default:
+			const _: never = type
+			return null
+	}
+}
+
+let backgroundTokenizer: { v: BackgroundTokenizer, editor: vscode.TextEditor, waitTokenUpdate: () => Promise<void> } | null = null
 
 export function activate(context: vscode.ExtensionContext) {
-	const webNakoServer = new WebNakoServer(context.extensionPath)
 	const selector: vscode.DocumentSelector = { language: "nadesiko3", scheme: undefined }
-	const diagnosticCollection = vscode.languages.createDiagnosticCollection("nadesiko3")
-	let panel: vscode.WebviewPanel | null = null
+	const nako3 = new NakoCompiler()
 
-	// シンタックスエラーの表示が速すぎるとコードを打っている最中に全体に赤線が引かれてしまうため、
-	// ドキュメントに変更があったら、その後0.5秒間変更が無い場合に限り、シンタックスエラーを表示する。
-	let diagnosticsCount = 0
-	const setDiagnosticsTimeout = () => {
-		diagnosticsCount++
-		const count = diagnosticsCount
-		setTimeout(() => {
-			if (count === diagnosticsCount) {
-				updateDiagnostics(diagnosticCollection)
-			}
-		}, 500);
-	}
-
-	const virtualDocuments = new Map<string, string>()
-
-	// overwrite = false の場合、呼ぶたびにメモリを消費していくことに注意
-	const showVirtualDocument = async (content: string, name: string, extension: string, overwrite: boolean, show: boolean): Promise<vscode.Uri> => {
-		let documentPath = `${name}${extension}`
-		if (!overwrite) {
-			let i = 2
-			while (virtualDocuments.has(documentPath)) {
-				documentPath = `${name}-${i}${extension}`
-				i++
-			}
-		}
-		virtualDocuments.set(documentPath, content)
-		const url = vscode.Uri.parse(`nadesiko3:${documentPath}`)
-		if (show) {
-			const document = await vscode.workspace.openTextDocument(url)
-			vscode.window.showTextDocument(document, { viewColumn: vscode.ViewColumn.Beside })
-		}
-		return url
-	}
-
-	const VSCodeRange = vscode.Range as unknown as abs.TypeofVSCodeRange
-
-	// 各プラグインの宣言ファイルを作る
-	const declarationFiles = Object.entries(mockPlugins).map(([name, plugin]) => createDeclarationFile(name, plugin))
-
-	const tokenDecorationType = vscode.window.createTextEditorDecorationType({
-		after: {
-			contentText: '|',
-			color: 'transparent',
-			width: '1px',
-		},
-		dark: {
-			after: {
-				backgroundColor: '#737373',
-			}
-		},
-		light: {
-			after: {
-				backgroundColor: '#bbbbbb',
-			}
-		}
-	})
-
-	const josiDecorationType = vscode.window.createTextEditorDecorationType({
-		textDecoration: "underline",
-	})
-
-	const languageFeatures = new LanguageFeatures(vscode.SemanticTokensBuilder, vscode.SemanticTokensLegend, declarationFiles, vscode.MarkdownString, vscode.Uri, true, VSCodeRange, vscode.Position)
-
-	const clearDecorations = (editor: vscode.TextEditor) => {
-		editor.setDecorations(tokenDecorationType, [])
-		editor.setDecorations(josiDecorationType, [])
-	}
-	languageFeatures.decorate = (document, decorations) => {
+	const onChange = () => {
 		const editor = vscode.window.activeTextEditor
-		if (editor === undefined) {
+		if (editor === undefined || editor === backgroundTokenizer?.editor) {
 			return
 		}
-		if (editor.document.languageId !== "nadesiko3") {
-			clearDecorations(editor)
+		if (backgroundTokenizer !== null) {
+			backgroundTokenizer.v.dispose()
+		}
+		if (editor.document.languageId !== 'nadesiko3') {
 			return
 		}
-		editor.setDecorations(tokenDecorationType, decorations.tokenDecorations as vscode.DecorationOptions[])
-		editor.setDecorations(josiDecorationType, decorations.josiDecorations as vscode.DecorationOptions[])
-	}
-
-	{
-		const editor = vscode.window.activeTextEditor
-		if (editor !== undefined) {
-			languageFeatures.onDidChange(editor.document)
+		let listeners = new Array<() => void>()
+		backgroundTokenizer = {
+			v: new BackgroundTokenizer(
+				new DocumentAdapter(editor),
+				nako3,
+				(firstRow, lastRow, ms) => {
+					listeners.forEach((f) => f())
+					listeners = []
+				},
+				(code, err) => { },
+			),
+			editor,
+			waitTokenUpdate: () => new Promise<void>((resolve) => { listeners.push(() => { resolve() }) })
 		}
 	}
-	setDiagnosticsTimeout()
+
+	const languageFeatures = new LanguageFeatures(AceRange, nako3)
 
 	context.subscriptions.push(
-		{ dispose() { virtualDocuments.clear() } },
-		webNakoServer,
-		diagnosticCollection,
-		vscode.workspace.registerTextDocumentContentProvider("nadesiko3-plugin-declaration", {
-			provideTextDocumentContent(uri: vscode.Uri): string {
-				const file = declarationFiles.find((file) => file.name === uri.path)
-				if (file === undefined) {
-					console.log(`プラグインの宣言ファイル ${uri.path} が見つかりません。`)
-					return ""
-				}
-				return file.content
+		vscode.window.onDidChangeActiveTextEditor(() => { onChange() }),
+		vscode.window.onDidChangeTextEditorOptions(() => { onChange() }),
+		vscode.workspace.onDidChangeConfiguration(() => { onChange() }),
+		vscode.workspace.onDidChangeTextDocument((e) => {
+			onChange()
+			if (backgroundTokenizer !== null) {
+				backgroundTokenizer.v.dirty = true
 			}
 		}),
-		vscode.workspace.registerTextDocumentContentProvider("nadesiko3", {
-			provideTextDocumentContent(uri: vscode.Uri): string {
-				return virtualDocuments.get(uri.path) || ""
-			}
-		}),
-		vscode.languages.registerCodeLensProvider(selector, codeLendsProvider),
-		vscode.languages.registerDocumentSemanticTokensProvider(selector, languageFeatures, languageFeatures.legend),
-		vscode.languages.registerDefinitionProvider(selector, new DefinitionProvider(declarationFiles)),
-		vscode.languages.registerDocumentHighlightProvider(selector, documentHighlightProvider),
-		vscode.window.onDidChangeActiveTextEditor((editor) => {
-			if (editor === undefined) {
-				return
-			}
-			if (editor.document.languageId !== "nadesiko3") {
-				clearDecorations(editor)
-			}
-			languageFeatures.onDidChange(editor.document)
-			setDiagnosticsTimeout()
-		}),
-		vscode.workspace.onDidChangeTextDocument((event) => {
-			const editor = vscode.window.activeTextEditor
-			if (editor === undefined) {
-				return
-			}
-			if (editor.document.languageId !== "nadesiko3") {
-				clearDecorations(editor)
-			}
-			languageFeatures.onDidChange(editor.document)
-			setDiagnosticsTimeout()
-		}),
-		vscode.workspace.onDidOpenTextDocument((document) => {
-			const editor = vscode.window.activeTextEditor
-			if (editor === undefined) {
-				return
-			}
-			if (editor.document.languageId !== "nadesiko3") {
-				clearDecorations(editor)
-			}
-			languageFeatures.onDidChange(editor.document)
-			setDiagnosticsTimeout()
-		}),
-		vscode.workspace.onDidCloseTextDocument((doc) => {
-			diagnosticCollection.delete(doc.uri)
-		}),
-		vscode.commands.registerCommand("nadesiko3.runActiveFileOnVSCode", () => {
-			const editor = vscode.window.activeTextEditor
-			if (editor === undefined) {
-				vscode.window.showErrorMessage("ファイルが開かれていません")
-				return
-			}
-			if (panel === null) {
-				panel = vscode.window.createWebviewPanel(
-					"nadesiko3Output",
-					"なでしこv3: プログラムの出力",
-					vscode.ViewColumn.Beside,
-					{
-						enableScripts: true,
-						localResourceRoots: [
-							vscode.Uri.file(path.join(context.extensionPath, "static"))
-						]
-					}
-				)
-			} else {
-				panel.reveal(vscode.ViewColumn.Beside)
-			}
-
-			{
-				const staticDir = path.join(context.extensionPath, "static")
-				panel.webview.html = fs.readFileSync(path.join(staticDir, "webview.html")).toString()
-					.replace("{index.css}", panel.webview.asWebviewUri(vscode.Uri.file(path.join(staticDir, 'index.css'))).toString())
-					.replace("{webview.js}", panel.webview.asWebviewUri(vscode.Uri.file(path.join(staticDir, 'webview.js'))).toString())
-					.replace("{index.js}", panel.webview.asWebviewUri(vscode.Uri.file(path.join(staticDir, 'index.js'))).toString())
-			}
-
-			panel.onDidDispose(() => {
-				panel = null
-			}, null, context.subscriptions)
-
-			const compiler = new NakoCompiler()
-			for (const plugin of [pluginNode, pluginCSV]) {
-				compiler.addPlugin(plugin)
-			}
-			// 「表示」の動作を上書き
-			compiler.setFunc('表示', [['と', 'を', 'の']], (s: any, sys: any) => {
-				if (panel === null) {
+		vscode.languages.registerDocumentSemanticTokensProvider(selector, {
+			provideDocumentSemanticTokens: async (document) => {
+				const tokensBuilder = new vscode.SemanticTokensBuilder(legend)
+				if (document !== backgroundTokenizer?.editor.document) {
 					return
 				}
-				// 文字列の場合はutil.inspectに掛けると''で囲まれてしまうため、そのまま送信
-				if (typeof s === "string") {
-					panel.webview.postMessage({ type: "out", line: s })
-					return
+				if (backgroundTokenizer.v.dirty) {
+					await backgroundTokenizer.waitTokenUpdate()
 				}
-				panel.webview.postMessage({ type: "out", line: util.inspect(s, { depth: null }) })
-			})
-			try {
-				// NOTE: 「N秒後」とかを使っていると関数を抜けた後も実行され続ける
-				compiler.runReset(editor.document.getText(), "")
-			} catch (e) {
-				panel.webview.postMessage({ type: "err", line: e.message })
-			}
-		}),
-		vscode.commands.registerCommand("nadesiko3.showJs", async () => {
-			const editor = vscode.window.activeTextEditor
-			if (editor === undefined) {
-				vscode.window.showErrorMessage("ファイルが開かれていません")
-				return
-			}
-			const compiler = new NakoCompiler()
-			for (const plugin of Object.values(mockPlugins)) {
-				compiler.addPlugin(plugin)
-			}
-			let content: string | null = null
-			try {
-				content = compiler.compile(editor.document.getText(), editor.document.fileName, false)
-			} catch (e) {
-				vscode.window.showErrorMessage(e.message)
-			}
-			if (content !== null) {
-				showVirtualDocument(content, "out", ".js", false, true)
-			}
-		}),
-		vscode.commands.registerCommand("nadesiko3.showBeautifiedJs", async () => {
-			const editor = vscode.window.activeTextEditor
-			if (editor === undefined) {
-				vscode.window.showErrorMessage("ファイルが開かれていません")
-				return
-			}
-			const compiler = new NakoCompiler()
-			for (const plugin of Object.values(mockPlugins)) {
-				compiler.addPlugin(plugin)
-			}
-			let content: string | null = null
-			try {
-				content = compiler.compile(editor.document.getText(), editor.document.fileName, false)
-			} catch (e) {
-				vscode.window.showErrorMessage(e.message)
-			}
-			if (content !== null) {
-				let code = jsBeutify.js(content, { end_with_newline: true })
-				code = code.replace(/;;+$/gm, ";") // 2重のセミコロンを適当に消す
-				showVirtualDocument(code, "out", ".js", false, true)
-			}
-		}),
-		vscode.commands.registerCommand('nadesiko3.showTokens', () => {
-			const editor = vscode.window.activeTextEditor
-			if (editor === undefined) {
-				vscode.window.showErrorMessage("ファイルが開かれていません")
-				return
-			}
-			const result = lex(editor.document.getText())
-			if (result instanceof LexError) {
-				vscode.window.showErrorMessage("コンパイルエラー")
-			} else {
-				showVirtualDocument(util.inspect(result.tokens, { depth: null }), "ast", ".js", false, true)
-			}
-		}),
-		vscode.commands.registerCommand('nadesiko3.showSimplifiedTokens', () => {
-			const editor = vscode.window.activeTextEditor
-			if (editor === undefined) {
-				vscode.window.showErrorMessage("ファイルが開かれていません")
-				return
-			}
-			const result = lex(editor.document.getText())
-			if (result instanceof LexError) {
-				vscode.window.showErrorMessage("コンパイルエラー")
-			} else {
-				for (const token of result.tokens) {
-					const obj = token as any
-					delete obj["file"]
-					delete obj["line"]
-					delete obj["column"]
-					delete obj["preprocessedCodeOffset"]
-					delete obj["preprocessedCodeLength"]
-					delete obj["startOffset"]
-					delete obj["endOffset"]
-					delete obj["rawJosi"]
-					if (typeof obj.meta === "object" && obj.meta !== null) {
-						delete obj.meta["declaration"]
-					}
-				}
-				showVirtualDocument(util.inspect(result.tokens, { depth: null }), "ast", ".js", false, true)
-			}
-		}),
-		vscode.commands.registerCommand('nadesiko3.showAst', () => {
-			const editor = vscode.window.activeTextEditor
-			if (editor === undefined) {
-				vscode.window.showErrorMessage("ファイルが開かれていません")
-				return
-			}
-			const result = parse(editor.document.getText())
-			if ("ok" in result) {
-				showVirtualDocument(util.inspect(result.ok, { depth: null }), "ast", ".js", false, true)
-			} else {
-				vscode.window.showErrorMessage("コンパイルエラー")
-			}
-		}),
-		vscode.commands.registerCommand('nadesiko3.showSimplifiedAst', () => {
-			const editor = vscode.window.activeTextEditor
-			if (editor === undefined) {
-				vscode.window.showErrorMessage("ファイルが開かれていません")
-				return
-			}
-			const result = parse(editor.document.getText())
-			if ("ok" in result) {
-				const simplifyAst = (ast: any): void => {
-					if (ast.type === "eol" || ast.type === "eof") {
-						delete ast["value"]
-						delete ast["josi"]
-					}
-					for (const key of ["line", "declaration", "column", "file", "preprocessedCodeOffset", "preprocessedCodeLength", "startOffset", "endOffset", "rawJosi"]) {
-						delete ast[key]
-					}
-					for (const key of Object.keys(ast)) {
-						if (typeof ast[key] === 'object' && ast[key] !== null && typeof ast[key].type === "string") {
-							simplifyAst(ast[key])
-						} else if (Array.isArray(ast[key])) {
-							for (const child of ast[key]) {
-								if (typeof child === "object" && child !== null && typeof child.type === "string") {
-									simplifyAst(child)
-								}
-							}
+				for (let line = 0; line < document.lineCount; line++) {
+					let character = 0
+					for (const token of backgroundTokenizer.v.getTokens(line)) {
+						const type = mapTokenType(token.type)
+						if (type !== null) {
+							tokensBuilder.push(new vscode.Range(line, character, line, character + token.value.length), ...type)
 						}
+						character += token.value.length
 					}
 				}
-				simplifyAst(result.ok)
-				showVirtualDocument(util.inspect(result.ok, { depth: null }), "ast", ".js", false, true)
-			} else {
-				vscode.window.showErrorMessage("コンパイルエラー")
+				return tokensBuilder.build()
 			}
-		}),
-		vscode.commands.registerCommand("nadesiko3.runActiveFileOnBrowser", () => {
-			const editor = vscode.window.activeTextEditor
-			if (editor === undefined) {
-				vscode.window.showErrorMessage("ファイルが開かれていません")
-				return
-			}
-			webNakoServer.runCode(editor.document.fileName, editor.document.getText())
-		}),
+		}, legend),
 	)
 }
 
-export function deactivate() { }
+export function deactivate() {
+	backgroundTokenizer?.v.dispose()
+}
--- static/webview.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener("message", (/** @type {{ data: { type: "out" | "err", line: string } }} */{ data }) => {
-    println(data.type, data.line)
-})
